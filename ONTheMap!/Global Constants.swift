23a17cc294fa205c4872501cb2e37ebc6f731618
Stephen Martinez <enharmonicnuance@gmail.com>
2017-03-03 16:57:13 -0800

    Added a Global Constants File for ease of use and Maintainability

---
+++ ONTheMap!/Global Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Global Constants.swift
+//  ONTheMap!
+//
+//  Created by Stephen Martinez on 3/3/17.
+//  Copyright © 2017 Stephen Martinez. All rights reserved.
+//
+/*
+This file contains the constant values that will be used widely throughout the application and are placed here for easy access and referencing as well as to avoid Magic Numbers. All Structs and Objects will contain the suffix Cnst (Standing for Constant).
+*/
+
+
+import Foundation
+import UIKit
+
+struct ParseCnst {
+    let AppID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+    let RESTApiKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+}
