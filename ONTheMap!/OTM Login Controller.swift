9837fccd8fce8bd82eab8f613d4c14cfe8779d64
Stephen Martinez <enharmonicnuance@gmail.com>
2017-04-05 18:26:54 -0700

    Added PinDropped Spring Animation and Key board notifacation with UITextfield delegates to the login controller.

--- ONTheMap!/OTM Login Controller.swift
+++ ONTheMap!/OTM Login Controller.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class OTMLoginController: UIViewController {
+class OTMLoginController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var OTMPin: UIImageView!
     @IBOutlet weak var OTMLogo: UIImageView!
-    
     
     @IBOutlet weak var loginTray: UIView!
     @IBOutlet weak var loginButton: UIButton!
@@ -23,32 +22,19 @@ class OTMLoginController: UIViewController {
     @IBOutlet weak var createUserAccnt: UIButton!
     @IBOutlet weak var cancelDot: UIButton!
     
-    
     @IBOutlet weak var visualEffect: UIVisualEffectView!
     
     override func viewDidLoad() { super.viewDidLoad()
         if let visualEffect = visualEffect{visualEffect.effect = nil}
+        userNameField.delegate = self
+        passwordField.delegate = self
     }
     
-    
-    
-    func animateLoginScreen(){
-        UIView.animate(withDuration: 0.8, animations: ({
-            self.OTMLogo.alpha = 1
-            self.loginTray.transform = CGAffineTransform(translationX: 0, y: -(self.loginButton.frame.height * 1.25))
-            self.loginButton.transform = CGAffineTransform(rotationAngle: ConvertObject.toRadians(180))
-        })){ successfulCompletion in
-            self.animatePinDrop()
-        }
+    override func viewWillAppear(_ animated: Bool) {super.viewWillAppear(true)
+        subscribeToKeyboardNotifications()
     }
     
-    func animatePinDrop(){
-        UIView.animate(withDuration: 0.35){
-            self.OTMPin.transform = CGAffineTransform(translationX: 0, y:(self.OTMPin.frame.height * 2.25))
-        }
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) { super.viewDidAppear(true)
         animateLoginScreen()
     }
     
@@ -56,8 +42,12 @@ class OTMLoginController: UIViewController {
         udacityLogin.translatesAutoresizingMaskIntoConstraints = true
         view.addSubview(udacityLogin)
         udacityLogin.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
-        udacityLogin.autoresizingMask = [UIViewAutoresizing.flexibleLeftMargin, UIViewAutoresizing.flexibleRightMargin, UIViewAutoresizing.flexibleTopMargin, UIViewAutoresizing.flexibleBottomMargin]
-        
+        udacityLogin.autoresizingMask = [
+            UIViewAutoresizing.flexibleLeftMargin,
+            UIViewAutoresizing.flexibleRightMargin,
+            UIViewAutoresizing.flexibleTopMargin,
+            UIViewAutoresizing.flexibleBottomMargin
+        ]
         UIView.animate(withDuration: 0.8){
             if let visualEffect = self.visualEffect{visualEffect.effect = UIBlurEffect(style: UIBlurEffectStyle.light)}
             self.udacityLogin.alpha = 1
@@ -78,6 +68,78 @@ class OTMLoginController: UIViewController {
         }
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if userNameField.text == nil || userNameField.text!.isBlank{userNameField.becomeFirstResponder() ;return false}
+        if passwordField.text == nil || passwordField.text!.isBlank{passwordField.becomeFirstResponder() ;return false}
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func keyboardWillShow(_ notification: Notification){
+        guard let keyboardFrame = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            else{print("Keyboard size not accesible"); return}
+        let clearanceScaler = CGFloat(userNameField.isEditing ? 2 : 1)
+        if udacityLogin.frame.intersects(keyboardFrame){
+            let moveBy = udacityLogin.frame.intersection(keyboardFrame)
+            view.frame.origin.y = 0 - (moveBy.height / clearanceScaler)
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if view.frame.origin.y != 0 {view.frame.origin.y = 0}
+    }
+    
+    func subscribeToKeyboardNotifications(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
     
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+extension OTMLoginController{
+    
+    func animateLoginScreen(){
+        UIView.animate(withDuration: 0.8, animations: ({
+            self.OTMLogo.alpha = 1
+            self.loginTray.transform = CGAffineTransform(translationX: 0, y: -(self.loginButton.frame.height * 1.25))
+            self.loginButton.transform = CGAffineTransform(rotationAngle: ConvertObject.toRadians(180))
+        })){ successfulCompletion in
+            self.animatePinDrop()
+        }
+    }
+    
+    func animatePinDrop(){
+        //Gives the dropped pin a springing effect once dropped.
+        UIView.animate(withDuration: 0.35, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: .curveLinear, animations: ({
+            self.OTMPin.transform = CGAffineTransform(translationX: 0, y:(self.OTMPin.frame.height * 2.25))
+        }), completion: nil)
+    }
+    
+    
+}
